933200a3c24271933192b8753898235499061a7c
Lucas Zorrer <lucaszorrer10@outlook.com>
2023-05-25 10:02:49 -0300

    Criação de Controller e views para produtos

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ProdutosController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/teste/{algo?}', function ($algo = null) {
-    return $algo;
-});
+Route::get('/produtos', [ProdutosController::class, 'index']);
 
-
-Route::get('/teste-view/{param?}', function ($param = null) {
-    return view('teste-view', [
-        'valor' => $param,
-    ]);
-});
