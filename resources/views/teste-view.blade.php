2547b4e56a7e53e214393c1b471b4c5a838d7f2a
Lucas Zorrer <lucaszorrer10@outlook.com>
2023-05-25 09:00:20 -0300

    Initial commit

---
+++ resources/views/teste-view.blade.php
@@ -0,0 +1,8 @@
+@extends('includes/base')
+
+@section('title', 'Título Joia')
+
+@section('content')
+    <p>Arquivo de Blade</p>
+    <p>Minha variável que veio de longe: {{ $valor }}</p>
+@endsection
